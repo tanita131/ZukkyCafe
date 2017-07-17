184fca96646cf214e13147ae6d417c157496c9a0
Tania Amparan <you@email.com>
2017-07-16 23:12:31 -0700

    animated nav menu and title

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+    //Menu effect//
+
+    $('.menu a').each(function(index, elemento){
+        $(this).css({
+            'top': '-200px'
+        });
+
+        $(this).animate({
+            top:'0'
+        }, 2000 + (index * 500));
+      
+    });
+
+    //Header effect//
+
+    if( $(window).width() > 800 ){
+        $('header .textos').css({
+            opacity: 0,
+            marginTop: 0
+        });
+
+         $('header .textos').animate({
+            opacity: 1,
+            marginTop: '-52px'
+        }, 1500);
+    }
+});
