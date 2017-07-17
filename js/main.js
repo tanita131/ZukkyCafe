d0a92e5908cc0d61643d876b761bbc6d51246df6
Tania Amparan <you@email.com>
2017-07-17 13:37:48 -0700

    created parallax effect

--- js/main.js
+++ js/main.js
@@ -26,4 +26,43 @@ $(document).ready(function(){
             marginTop: '-52px'
         }, 1500);
     }
+
+    // Smooth scrolling//
+
+    var acercaDe = $('#acerca-de').offset().top,
+        menu = $('#platillos').offset().top,
+        galeria = $('#galeria').offset().top,
+        ubicacion = $('#ubicacion').offset().top;
+
+    $('#btn-acerca-de').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop:acercaDe -100
+        }, 500);
+    });
+
+        $('#btn-menu').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: menu
+        }, 500);
+    });
+
+        $('#btn-galeria').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: galeria
+        }, 500);
+    });
+
+        $('#btn-ubicacion').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: ubicacion
+        }, 500);
+    });
+
+
+
+
 });
